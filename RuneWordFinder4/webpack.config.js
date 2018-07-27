3f2eb8811928e41b68480797b67edf78d5973a9a
BenedictRasmussen <benedictrasmussen@gmail.com>
2018-07-26 19:56:10 -0500

    Added babel to transpile es6 down, added rule to webpack to transpile js

--- RuneWordFinder4/webpack.config.js
+++ RuneWordFinder4/webpack.config.js
@@ -7,11 +7,26 @@ module.exports = {
         path: path.resolve(__dirname, 'wwwroot/dist'),
         filename: 'bundle.js'
     },
+
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         })
-    ]
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
