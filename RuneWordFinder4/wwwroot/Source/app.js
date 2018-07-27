c7d26f6ff5130d43d999049a51188496ddaef88e
BenedictRasmussen <benedictrasmussen@gmail.com>
2018-07-26 22:39:08 -0500

    Added basic react component

--- RuneWordFinder4/wwwroot/Source/app.js
+++ RuneWordFinder4/wwwroot/Source/app.js
@@ -2,9 +2,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/site.css';
 
-$('#fillthiswithjquery').html('Filled by Jquery!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './reactcomponent';
 
+ReactDOM.render(
+    <Counter />,
+    document.getElementById('basicreactcomponent')
+);
+
+/*
+$('#fillthiswithjquery').html('Filled by Jquery!');
 let myES6Object = new ES6Lib();
 $('#fillthiswithes6lib').html(myES6Object.getData());
+*/
 
 module.hot.accept();
