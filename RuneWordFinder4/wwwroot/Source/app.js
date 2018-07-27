6e0d7b809d265fc4bba16746b22a83bb4451f70f
BenedictRasmussen <benedictrasmussen@gmail.com>
2018-07-26 21:16:27 -0500

    Removed defunct lib.js, added bootstrap

--- RuneWordFinder4/wwwroot/Source/app.js
+++ RuneWordFinder4/wwwroot/Source/app.js
@@ -1,7 +1,7 @@
-﻿require('./lib');
-import ES6Lib from './es6codelib';
+﻿import ES6Lib from './es6codelib';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/site.css';
 
-document.getElementById("fillthis").innerHTML = getText();
 $('#fillthiswithjquery').html('Filled by Jquery!');
 
 let myES6Object = new ES6Lib();
