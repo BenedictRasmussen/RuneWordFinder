c28a7172e1fdbc62345d24da7ba2f96ecf79b94b
BenedictRasmussen <benedictrasmussen@gmail.com>
2018-08-04 14:35:08 -0500

    Added basic RuneController

---
+++ RuneWordFinder4/Controllers/RuneController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RuneWordFinder4.Models.Repository;
+namespace RuneWordFinder4.Controllers
+{
+    public class RuneController : Controller
+    {
+        private readonly MongoDataService dataService = new MongoDataService();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View(dataService.FindRunes());
+        }
+    }
+}
