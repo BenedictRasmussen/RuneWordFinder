198435ff458b9e0621dce92fc7b24e709acd86c4
BenedictRasmussen <benedictrasmussen@gmail.com>
2019-02-26 23:37:05 -0600

    Update runewords model to match seed

--- RuneWordFinder4/Models/Runewords.cs
+++ RuneWordFinder4/Models/Runewords.cs
@@ -10,12 +10,16 @@ namespace RuneWordFinder4.Models
         public ObjectId ObjectId { get; set; }
         [BsonElement("name")]
         public string Name { get; set; }
+        [BsonElement("version")]
+        public string Version { get; set; }
         [BsonElement("type")]
         public string Type { get; set; }
         [BsonElement("runeName")]
         public string RuneName { get; set; }
         [BsonElement("runes")]
         public IEnumerable<string> Runes { get; set; }
+        [BsonElement("sockets")]
+        public int Sockets { get; set; }
         [BsonElement("stats")]
         public IEnumerable<string> Stats { get; set; }
     }
