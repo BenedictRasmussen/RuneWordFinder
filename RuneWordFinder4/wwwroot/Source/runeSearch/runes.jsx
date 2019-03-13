c743a81ec6ecb47f68d20c081c4a923debee3fca
BenedictRasmussen <benedictrasmussen@gmail.com>
2019-03-12 22:42:18 -0500

    Updated runewords to match new model

--- RuneWordFinder4/wwwroot/Source/runeSearch/runes.jsx
+++ RuneWordFinder4/wwwroot/Source/runeSearch/runes.jsx
@@ -15,14 +15,14 @@ const Runes = props => {
                 <h3>Runes lvl 1 - 21</h3>
                 {
                     runeGroupA.map(rune =>
-                           <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox } key={ rune.name }></Rune>)
+                           <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
                 }
             </div>
             <div className="rune-col">
                 <h3 >Runes lvl 22 - 41</h3>
                 {
                     runeGroupB.map(rune =>
-                            <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox } key={ rune.name }></Rune>)
+                            <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
                 }
             </div>
         </div>
@@ -31,14 +31,14 @@ const Runes = props => {
                 <h3>Runes lvl 42 - 61</h3>
                 {
                     runeGroupC.map(rune =>
-                            <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox } key={ rune.name }></Rune>)
+                            <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
                 }
             </div>
             <div className="rune-col">
                 <h3>Runes lvl 61+</h3>
                 {
                     runeGroupD.map(rune =>
-                            <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox } key={ rune.name }></Rune>)
+                            <Rune rune={ rune } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
                 }
             </div>
         </div>
--- RuneWordFinder4/wwwroot/Source/runeSearch/runeword.jsx
+++ RuneWordFinder4/wwwroot/Source/runeSearch/runeword.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Runeword = props => (
     <div>
         <h2>{ props.runeword.name }</h2>
-        <h3>{ props.runeword.runeName }</h3>
+        <h3>{ props.runeword.runes.map(rune => rune.charAt(0).toUpperCase() + rune.slice(1)).join('') }</h3>
+        <p><b>Equipment Type:</b> {props.runeword.runes.length} socketed { props.runeword.type }</p>
         <ul>{ props.runeword.stats.map(statLine => <li>{statLine}</li>) }</ul>
     </div>
 )
