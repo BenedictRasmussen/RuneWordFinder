b9d221f319799aa5dfeebe2d92e5cd0b2e0158aa
BenedictRasmussen <benedictrasmussen@gmail.com>
2019-03-31 15:43:24 -0500

    Added vague color scheme

--- RuneWordFinder4/wwwroot/Source/runeSearch/runes.jsx
+++ RuneWordFinder4/wwwroot/Source/runeSearch/runes.jsx
@@ -3,23 +3,23 @@ import Rune from './rune.jsx'
 
 const Runes = props => {
     console.log(props.runes)
-    let runeGroupA = props.runes.filter(rune => rune.level > 0 && rune.level <= 21);
-    let runeGroupB = props.runes.filter(rune => rune.level > 21 && rune.level <= 41);
-    let runeGroupC = props.runes.filter(rune => rune.level > 41 && rune.level <= 61);
-    let runeGroupD = props.runes.filter(rune => rune.level > 61);
+    let runeGroupA = props.runes.filter(rune => rune.level > 0 && rune.level <= 19);
+    let runeGroupB = props.runes.filter(rune => rune.level > 19 && rune.level <= 39);
+    let runeGroupC = props.runes.filter(rune => rune.level > 39 && rune.level <= 59);
+    let runeGroupD = props.runes.filter(rune => rune.level > 59);
 
     return (
         <div>
         <div className="rune-row">
             <div className="rune-col">
-                <h3>Runes lvl 1 - 21</h3>
+                <h3>Runes lvl 0 - 19</h3>
                 {
                     runeGroupA.map(rune =>
                            <Rune rune={ rune } key={ rune.name } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
                 }
             </div>
             <div className="rune-col">
-                <h3 >Runes lvl 22 - 41</h3>
+                <h3 >Runes lvl 19 - 39</h3>
                 {
                     runeGroupB.map(rune =>
                             <Rune rune={ rune } key={ rune.name } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
@@ -28,14 +28,14 @@ const Runes = props => {
         </div>
         <div className="rune-row">
             <div className="rune-col">
-                <h3>Runes lvl 42 - 61</h3>
+                <h3>Runes lvl 39 - 59</h3>
                 {
                     runeGroupC.map(rune =>
                             <Rune rune={ rune } key={ rune.name } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
                 }
             </div>
             <div className="rune-col">
-                <h3>Runes lvl 61+</h3>
+                <h3>Runes lvl 59+</h3>
                 {
                     runeGroupD.map(rune =>
                             <Rune rune={ rune } key={ rune.name } handleCheckboxChange={ props.toggleCheckbox }></Rune>)
