77aea4fa93778d7b7f3d8a95cf19d848eabf5c92
BenedictRasmussen <benedictrasmussen@gmail.com>
2019-03-31 16:21:23 -0500

    Modified content margin handling, much cleaner

--- RuneWordFinder4/wwwroot/Source/runeSearch/runeword.jsx
+++ RuneWordFinder4/wwwroot/Source/runeSearch/runeword.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Runeword = props => (
-    <div>
-        <h2>{ props.runeword.name }</h2>
-        <h3>{ props.runeword.runes.map(rune => rune.charAt(0).toUpperCase() + rune.slice(1)).join('') }</h3>
+    <div className="runeword">
+        <h1>{ props.runeword.name }</h1>
+        <h2>{ props.runeword.runes.map(rune => rune.charAt(0).toUpperCase() + rune.slice(1)).join(' ') }</h2>
         <p><b>Equipment Type:</b> {props.runeword.runes.length} socketed { props.runeword.type }</p>
         <ul>{ props.runeword.stats.map(statLine => <li>{statLine}</li>) }</ul>
     </div>
